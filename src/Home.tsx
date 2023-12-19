c6d7d8c2c2238fd0ecffd6b29dd5a80268bf1895
sgayathrii <sgayathrii@gmail.com>
2023-12-19 11:14:47 +0100

    Added home image

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,24 @@
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import supabase from './libs/supabase';
-import { useContext } from 'react';
-import StoreContext from './contexts/Store';
-import AnimatedDiv from './components/AnimatedDiv';
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import supabase from "./libs/supabase";
+import { useContext } from "react";
+import StoreContext from "./contexts/Store";
+import AnimatedDiv from "./components/AnimatedDiv";
+import homeImage from "../src/asserts/home.jpg";
 
 export default function Home() {
   const { session } = useContext(StoreContext);
 
   return (
-    <AnimatedDiv className="home-parent">
-      <section className="home-block1">Welcome !</section>
-      <section className="home-block2">
+    <AnimatedDiv className="home">
+     <main className="home-container">
+      <div className="home-container__welcome">
+        
+        <img src={homeImage} alt="home image" className="home-container__welcome__image"/>
+      </div>
+
+      <div className="home-container__login">
+       <span>Welcome Back!</span>
         {!session && (
           <Auth
             supabaseClient={supabase}
@@ -19,7 +26,8 @@ export default function Home() {
             providers={[]}
           />
         )}
-      </section>
+      </div>
+      </main>
     </AnimatedDiv>
   );
 }
