fb4ab66b1313a0b06b54b8cf05882fdc18e3564e
Johan Abde <abdejohan@gmail.com>
2023-12-20 11:26:07 +0100

    Add mockupdata for facilities

---
+++ mockups/mockedFacilities.ts
@@ -0,0 +1,34 @@
+const mockedFacilities = [
+  {
+    id: 1,
+    name: 'Laundry Room',
+    image: 'laundry-room.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sauna',
+    image: 'sauna.jpg',
+  },
+  {
+    id: 3,
+    name: 'Padel Court',
+    image: 'padel-court.jpg',
+  },
+  {
+    id: 4,
+    name: 'Roof Balcony',
+    image: 'roof-balcony.jpg',
+  },
+  {
+    id: 5,
+    name: 'Event Hall',
+    image: 'event-hall.jpg',
+  },
+  {
+    id: 6,
+    name: 'Guest Room',
+    image: 'guest-room.jpg',
+  },
+];
+
+export default mockedFacilities;
