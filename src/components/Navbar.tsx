c0cf3fb9d8573678ee3882f03318adada5971707
Johan Abde <abdejohan@gmail.com>
2023-12-20 10:50:19 +0100

    Refactor styling with  sass

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,24 +2,28 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import StoreContext from '../contexts/Store';
 import supabase from '../libs/supabase';
+import { UserOutlined } from '@ant-design/icons';
 
 const Navbar = () => {
   const { session } = useContext(StoreContext);
 
   return (
-    <nav>
-      <ul
-        style={{
-          display: 'flex',
-          listStyleType: 'none',
-          gap: 20,
-        }}>
-        <li>
-          <Link to={'/vite-app/'}>Home</Link>
-        </li>
-        <li>
-          <Link to={'/vite-app/Dashboard'}>Dashboard</Link>
-        </li>
+    <nav className="navigation">
+      <ul className="list">
+        <span>logo</span>
+        <span>
+          <UserOutlined />
+        </span>
+        {false && (
+          <>
+            <li>
+              <Link to={'/vite-app/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/vite-app/Dashboard'}>Dashboard</Link>
+            </li>
+          </>
+        )}
         {session && (
           <li>
             <Link to={'/vite-app/'} onClick={() => supabase.auth.signOut()}>
