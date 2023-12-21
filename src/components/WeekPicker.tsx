c26b536827c1b80f1bac910e6d8e7deb99d7cc6f
sgayathrii <sgayathrii@gmail.com>
2023-12-21 09:50:46 +0100

    New design to week calendar in laundry

--- src/components/WeekPicker.tsx
+++ src/components/WeekPicker.tsx
@@ -1,84 +1,152 @@
-import React, { useState, useEffect } from "react";
-import { startOfWeek, addDays, format, isAfter } from "date-fns";
-import { LeftCircleTwoTone, RightCircleTwoTone } from "@ant-design/icons";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import {
+  format,
+  subMonths,
+  addMonths,
+  startOfWeek,
+  addDays,
+  isSameDay,
+  lastDayOfWeek,
+  getWeek,
+  addWeeks,
+  subWeeks
+} from "date-fns";
 
-interface WeekPickerProps {
-  currentWeekNumber: number;
-  onWeekChange: (weekNumber: number) => void;
+type WeekPickerProps = {
+  showDetailsHandle: (dayStr: string) => void;
 }
 
-const WeekPicker: React.FC<WeekPickerProps> = ({
-  currentWeekNumber,
-  onWeekChange,
-}) => {
-  const [currentWeek, setCurrentWeek] = useState<Date[]>([]);
-  const [displayedWeekNumber, setDisplayedWeekNumber] =
-    useState<number>(currentWeekNumber);
 
-  useEffect(() => {
-    const startDate = startOfWeek(new Date(), { weekStartsOn: 1 }); // Assuming Monday is the start of the week
-    const weekDays = Array.from({ length: 7 }, (_, index) =>
-      addDays(startDate, index)
-    );
-    setCurrentWeek(weekDays);
-  }, []);
+const WeekPicker = ({ showDetailsHandle }:WeekPickerProps) => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [currentWeek, setCurrentWeek] = useState(getWeek(currentMonth));
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const handleNextWeek = () => {
-    const nextWeekNumber = displayedWeekNumber + 1;
-    const nextWeekStartDate = startOfWeek(addDays(currentWeek[0], 7), {
-      weekStartsOn: 1,
-    });
-
-    if (isAfter(nextWeekStartDate, new Date())) {
-      onWeekChange(nextWeekNumber);
-      const nextWeekDays = Array.from({ length: 7 }, (_, index) =>
-        addDays(nextWeekStartDate, index)
-      );
-      setCurrentWeek(nextWeekDays);
-      setDisplayedWeekNumber(nextWeekNumber);
+  const changeMonthHandle = (btnType: "prev" | "next") => {
+    if (btnType === "prev") {
+      setCurrentMonth(subMonths(currentMonth, 1));
+    }
+    if (btnType === "next") {
+      setCurrentMonth(addMonths(currentMonth, 1));
     }
   };
 
-  const handlePrevWeek = () => {
-    const prevWeekNumber = displayedWeekNumber - 1;
-    onWeekChange(prevWeekNumber);
-
-    const prevWeekStartDate = startOfWeek(addDays(currentWeek[0], -7), {
-      weekStartsOn: 1,
-    });
-    if (isAfter(prevWeekStartDate, new Date())) {
-      onWeekChange(prevWeekNumber);
-      const prevWeekDays = Array.from({ length: 7 }, (_, index) =>
-        addDays(prevWeekStartDate, index)
-      );
-      setCurrentWeek(prevWeekDays);
-      setDisplayedWeekNumber(prevWeekNumber);
+  const changeWeekHandle = (btnType: "prev" | "next") => {
+    //console.log("current week", currentWeek);
+    if (btnType === "prev") {
+      //console.log(subWeeks(currentMonth, 1));
+      setCurrentMonth(subWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(subWeeks(currentMonth, 1)));
+    }
+    if (btnType === "next") {
+      //console.log(addWeeks(currentMonth, 1));
+      setCurrentMonth(addWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(addWeeks(currentMonth, 1)));
     }
   };
 
-  return (
-    <div className="week-calendar">
-      <div className="week-calendar__content">
-        <LeftCircleTwoTone
-          onClick={handlePrevWeek}
-          className="week-calendar__arrow-icon"
-        />
-        <span className="week-calendar__week-number">
-          Week: {displayedWeekNumber}
-        </span>
-        <RightCircleTwoTone
-          onClick={handleNextWeek}
-          className="week-calendar__arrow-icon"
-        />
+  const onDateClickHandle = (day: Date, dayStr: string) => {
+    setSelectedDate(day);
+    showDetailsHandle(dayStr);
+  };
+
+  const renderHeader = () => {
+    const dateFormat = "MMM yyyy";
+    // console.log("selected day", selectedDate);
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          {/* <div className="icon" onClick={() => changeMonthHandle("prev")}>
+            prev month
+          </div> */}
+        </div>
+        <div className="col col-center">
+          <span>{format(currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end">
+          {/* <div className="icon" onClick={() => changeMonthHandle("next")}>next month</div> */}
+        </div>
       </div>
+    );
+  };
+  const renderDays = () => {
+    const dateFormat = "EEE";
+    const days = [];
+    const startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {format(addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+    return <div className="days row">{days}</div>;
+  };
+  const renderCells = () => {
+    const startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    const endDate = lastDayOfWeek(currentMonth, { weekStartsOn: 1 });
+    const dateFormat = "d";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              isSameDay(day, new Date())
+                ? "today"
+                : isSameDay(day, selectedDate)
+                ? "selected"
+                : ""
+            }`}
+            key={+cloneDay} 
+            onClick={() => {
+              const dayStr = format(cloneDay, "ccc dd MMM yy");
+              onDateClickHandle(cloneDay, dayStr);
+            }}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = addDays(day, 1);
+      }
 
-      <ul className="week-calendar__weekdays">
-        {currentWeek.map((day, index) => (
-          <li key={index} className="custom-li-class">
-            {format(day, "eeee, MMM dd")}
-          </li>
-        ))}
-      </ul>
+      rows.push(
+        <div className="row" key={+day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  };
+  const renderFooter = () => {
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={() => changeWeekHandle("prev")}>
+            prev week
+          </div>
+        </div>
+        <div>{currentWeek}</div>
+        <div className="col col-end" onClick={() => changeWeekHandle("next")}>
+          <div className="icon">next week</div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="calendar">
+      {renderHeader()}
+      {renderDays()}
+      {renderCells()}
+      {renderFooter()}
     </div>
   );
 };
--- src/pages/Laundry.tsx
+++ src/pages/Laundry.tsx
@@ -1,21 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import AnimatedDiv from "../components/AnimatedDiv";
 import WeekPicker from "../components/WeekPicker";
-import { getISOWeek } from "date-fns";
-const Laundry = () => {
-  const [currentWeekNumber, setCurrentWeekNumber] = useState<number>(
-    getISOWeek(new Date())
-  );
 
-  const handleWeekChange = (weekNumber: number) => {
-    setCurrentWeekNumber(weekNumber);
+const Laundry = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  const [data, setData] = useState<string | null>(null);
+
+  const showDetailsHandle = (dayStr: string) => {
+    setData(dayStr);
+    setShowDetails(true);
   };
 
   return (
     <AnimatedDiv>
       <WeekPicker
-        currentWeekNumber={currentWeekNumber}
-        onWeekChange={handleWeekChange}
+        showDetailsHandle={showDetailsHandle} 
       />
     </AnimatedDiv>
   );
