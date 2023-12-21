3f39a7f20871ee5c968070dfecdae10369080e58
Johan Abde <abdejohan@gmail.com>
2023-12-21 22:00:53 +0100

    Remove eslint warning for empty dep array in useEffect

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
