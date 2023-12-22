1c4159da0df1580188a3977cb778f52471349148
Johan Abde <abdejohan@gmail.com>
2023-12-22 10:33:36 +0100

    Replace all mockups with real data for facilities

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,17 @@
 import AnimatedDiv from '../components/AnimatedDiv';
 import 'react-day-picker/dist/style.css';
-import mockedFacilities from '../../mockups/mockedFacilities.ts';
 import { Link } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore.ts';
+import { useFacilityStore } from '../store/facilityStore.ts';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
   const { user } = useAuthStore();
+  const { fetchFacilities, facilities } = useFacilityStore();
+
+  useEffect(() => {
+    fetchFacilities();
+  }, []);
 
   return (
     <AnimatedDiv>
@@ -13,7 +19,7 @@ const Dashboard = () => {
       <span>Apartment: #{user?.apartmentNumber}</span>
       <h4 className="grid-title">Facilites</h4>
       <div className="grid-container">
-        {mockedFacilities.map(({ id, name }) => (
+        {facilities?.map(({ id, name }) => (
           <div className="grid-item" key={id}>
             <Link to={`/vite-app/facility/${id}`}>{name}</Link>
           </div>
--- src/pages/Facility.tsx
+++ src/pages/Facility.tsx
@@ -1,7 +1,6 @@
 import AnimatedDiv from '../components/AnimatedDiv';
 import 'react-day-picker/dist/style.css';
 import SlotPicker from '../components/SlotPicker';
-import image from '../assets/home.jpg';
 import WeekPicker from '../components/WeekPicker';
 import { useParams } from 'react-router-dom';
 import { useFacilityStore } from '../store/facilityStore';
@@ -19,7 +18,11 @@ const Facility = () => {
   return (
     <AnimatedDiv>
       <div className="image-container">
-        <img src={image} alt={facility?.name} className="facility__image" />
+        <img
+          src={facility?.image}
+          alt={facility?.name}
+          className="facility__image"
+        />
       </div>
       {facility && (
         <>
