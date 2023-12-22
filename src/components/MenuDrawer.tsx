c30a1bd568a8ad031988350c50d4d9361ea4ffeb
sgayathrii <sgayathrii@gmail.com>
2023-12-22 19:44:24 +0100

    Added styling to drawerMenu

--- src/components/MenuDrawer.tsx
+++ src/components/MenuDrawer.tsx
@@ -17,32 +17,49 @@ const MenuDrawer = ({ visible, onClose }: MenuDrawerProps) => {
 
   return (
     <Drawer
-      title="Basic Drawer"
+      title="Quick Menu"
       placement="right"
       onClose={onClose}
       open={visible}>
       <ul className="menu-list">
-        <li>
-          <Link to={'/vite-app/'} onClick={handleLinkClick}>
+        <li className="menu-list__item">
+          <Link
+            to={'/vite-app/'}
+            onClick={handleLinkClick}
+            className="menu-list__link">
             Home
           </Link>
         </li>
-        <li>
-          <Link to={'/vite-app/Dashboard'} onClick={handleLinkClick}>
+        <li className="menu-list__item">
+          <Link
+            to={'/vite-app/Dashboard'}
+            onClick={handleLinkClick}
+            className="menu-list__link">
             Dashboard
           </Link>
         </li>
         {session && (
-          <li>
-            <Link
-              to={'/vite-app/'}
-              onClick={() => {
-                supabase.auth.signOut();
-                handleLinkClick();
-              }}>
-              Sign out
-            </Link>
-          </li>
+          <>
+            <li className="menu-list__item">
+              <Link
+                to={'/vite-app/bookings'}
+                onClick={handleLinkClick}
+                className="menu-list__link">
+                All bookings
+              </Link>
+            </li>
+            <li className="menu-list__item">
+              <Link
+                to={'/vite-app/'}
+                onClick={() => {
+                  supabase.auth.signOut();
+                  handleLinkClick();
+                }}
+                className="menu-list__link">
+                Sign out
+              </Link>
+            </li>
+          </>
         )}
       </ul>
     </Drawer>
