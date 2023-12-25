f51bd7d6bbe5dc9a92fc908dcf365a816afe277b
Johan Abde <abdejohan@gmail.com>
2023-12-25 10:36:03 +0100

    Add a primary button for making a booking

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,14 +2,14 @@ import AnimatedDiv from '../components/AnimatedDiv';
 import 'react-day-picker/dist/style.css';
 import UserProfile from '../components/UserProfile.tsx';
 import ActiveBookings from '../components/ActiveBookings.tsx';
-import AllFacilities from '../components/AllFacilities.tsx';
+import MakeABooking from '../components/MakeABooking.tsx';
 
 const Dashboard = () => {
   return (
     <AnimatedDiv>
       <UserProfile />
       <ActiveBookings />
-      <AllFacilities />
+      <MakeABooking />
     </AnimatedDiv>
   );
 };
