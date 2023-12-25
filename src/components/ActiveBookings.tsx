b9f75b4ce36aa1fe57d80c745d5d6b2811008caa
Johan Abde <abdejohan@gmail.com>
2023-12-25 14:54:25 +0100

    Replace mockup bookings data with real data

--- src/components/ActiveBookings.tsx
+++ src/components/ActiveBookings.tsx
@@ -1,20 +1,22 @@
 import { Card } from 'antd';
-import mockedBookings from '../../mockups/mockedBookings';
 import { describeDate, formatDate, formatTime, getDayOfWeek } from '../utils';
 import { EditOutlined } from '@ant-design/icons';
+import { useAuthStore } from '../store/authStore';
 
 const ActiveBookings = () => {
+  const { user } = useAuthStore();
+
   return (
     <div className="active-bookings">
       <h6>Active Bookings</h6>
-      {mockedBookings.length > 0 ? (
-        mockedBookings.map((booking) => (
+      {user && user?.bookings.length > 0 ? (
+        user.bookings.map((booking) => (
           <Card
             key={booking.id}
             size="small"
             title={
               <div className="booking__title">
-                <span>{booking.facility.name}</span>
+                <span>{booking.facilityName}</span>
                 <span className="booking__weekday">
                   {describeDate(booking.date)}
                 </span>
