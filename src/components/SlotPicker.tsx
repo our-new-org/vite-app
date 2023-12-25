eabde0cbf85d89163e6a145c4df81e49d2237a5c
Johan Abde <abdejohan@gmail.com>
2023-12-25 13:13:10 +0100

    Add fade out/in effect to slot picker

--- src/components/SlotPicker.tsx
+++ src/components/SlotPicker.tsx
@@ -2,7 +2,8 @@ import { addMinutes } from 'date-fns';
 import Slot from './Slot';
 import { useFacilityStore } from '../store/facilityStore';
 import { combineDateAndTime } from '../utils';
-import { useDatePickerStore } from '../store/DatePickerStore';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useDatePickerStore } from '../store/datePickerStore';
 
 interface SlotPickerProps {}
 
@@ -20,24 +21,33 @@ const SlotPicker: React.FC<SlotPickerProps> = () => {
   }
 
   return (
-    <section className="facility">
-      <h1 className="facility__title">Available Slots</h1>
-      <ul className="slot-list">
-        {slots?.map((slot, index) => (
-          <Slot
-            slot={slot}
-            key={index}
-            slotDuration={facility!.slotDuration}
-            handleSlotSelect={(slot) => {
-              slot.setSeconds(0);
-              slot.setMilliseconds(0);
-              setSelectedSlot(slot);
-            }}
-            selectedSlot={selectedSlot}
-          />
-        ))}
-      </ul>
-    </section>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={selectedDate.getTime()} // UNIX date
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.1 }}>
+        <section className="facility">
+          <h1 className="facility__title">Available Slots</h1>
+          <ul className="slot-list">
+            {slots?.map((slot, index) => (
+              <Slot
+                slot={slot}
+                key={index}
+                slotDuration={facility!.slotDuration}
+                handleSlotSelect={(slot) => {
+                  slot.setSeconds(0);
+                  slot.setMilliseconds(0);
+                  setSelectedSlot(slot);
+                }}
+                selectedSlot={selectedSlot}
+              />
+            ))}
+          </ul>
+        </section>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
