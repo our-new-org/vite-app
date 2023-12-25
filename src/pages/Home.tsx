9798e2f294978be190e6db1a0dc9404a7e963835
Johan Abde <abdejohan@gmail.com>
2023-12-25 23:11:47 +0100

    Refactor home page css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import AnimatedDiv from '../components/AnimatedDiv';
 import homeImage from '../assets/home.jpg';
 import { useAuth } from '../hooks/useAuth';
 import { useAuthStore } from '../store/authStore';
+import { Button } from 'antd';
 
 export default function Home() {
   const { session } = useAuthStore();
@@ -14,16 +15,21 @@ export default function Home() {
     <AnimatedDiv className="home">
       <main className="home-container">
         <div className="home-container__welcome">
-          <button
-            onClick={scrollToLogin}
-            className="home-container__welcome__loginButton">
-            Login
-          </button>
           <img
             src={homeImage}
             alt="home image"
             className="home-container__welcome__image"
           />
+          <div className="home-container__welcome__login-button">
+            <Button
+              type="primary"
+              size="large"
+              block
+              onClick={scrollToLogin}
+              className="shadow">
+              Login
+            </Button>
+          </div>
         </div>
 
         <div className="home-container__login">
@@ -36,10 +42,12 @@ export default function Home() {
                 className: { container: 'auth-container login-scroll' },
                 style: {
                   container: { margin: '8px', color: 'white' },
-                  anchor: { color: '#38665F', margin: '10px' },
+                  anchor: { color: '#e99ea9', margin: '10px' },
                   button: {
-                    backgroundColor: '#38665F',
-                    borderColor: '#5D5C57',
+                    backgroundColor: '#e99ea9',
+                    borderColor: '#e99ea9',
+                    boxShadow:
+                      '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
                     color: 'white',
                     borderRadius: '5px',
                   },
