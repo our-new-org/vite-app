85f1267b3f7c0f240e2fb66d0b458c90e0a50a2e
Johan Abde <abdejohan@gmail.com>
2023-12-25 11:40:07 +0100

    Display info text if there is no active bookings

--- src/components/ActiveBookings.tsx
+++ src/components/ActiveBookings.tsx
@@ -7,29 +7,35 @@ const ActiveBookings = () => {
   return (
     <div className="active-bookings">
       <h6>Active Bookings</h6>
-      {mockedBookings.map((booking) => (
-        <Card
-          key={booking.id}
-          size="small"
-          title={
-            <div className="booking__title">
-              <span>{booking.facility.name}</span>
-              <span className="booking__weekday">
-                {describeDate(booking.date)}
-              </span>
-            </div>
-          }
-          extra={<EditOutlined className="booking-edit" size={100} />}>
-          <p>
-            <span className="booking__label">When:</span>{' '}
-            {getDayOfWeek(booking.date)} {formatDate(booking.date)}
-          </p>
-          <p>
-            <span className="booking__label">Time:</span>{' '}
-            {formatTime(booking.startTime)} - {formatTime(booking.endTime)}
-          </p>
-        </Card>
-      ))}
+      {mockedBookings.length > 0 ? (
+        mockedBookings.map((booking) => (
+          <Card
+            key={booking.id}
+            size="small"
+            title={
+              <div className="booking__title">
+                <span>{booking.facility.name}</span>
+                <span className="booking__weekday">
+                  {describeDate(booking.date)}
+                </span>
+              </div>
+            }
+            extra={<EditOutlined className="booking-edit" size={100} />}>
+            <p>
+              <span className="booking__label">When:</span>{' '}
+              {getDayOfWeek(booking.date)} {formatDate(booking.date)}
+            </p>
+            <p>
+              <span className="booking__label">Time:</span>{' '}
+              {formatTime(booking.startTime)} - {formatTime(booking.endTime)}
+            </p>
+          </Card>
+        ))
+      ) : (
+        <span className="booking__no-active">
+          You have no active bookings..
+        </span>
+      )}
     </div>
   );
 };
