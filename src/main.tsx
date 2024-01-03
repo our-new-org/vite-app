8c2398e8bb9b03f8a554f13710018caf33317116
Johan Abde <abdejohan@gmail.com>
2024-01-03 16:05:27 +0100

    Work on hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AnimatedRoutes from './components/AnimatedRoutes.tsx';
 import Navbar from './components/Navbar.tsx';
 import { ConfigProvider } from 'antd';
@@ -10,10 +10,10 @@ const baseUrl = import.meta.env.PROD ? '/vite-app' : '/';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ConfigProvider
     theme={{ token: { colorPrimary: '#e99ea9', borderRadius: 5 } }}>
-    <BrowserRouter basename={baseUrl}>
+    <HashRouter>
       <Navbar />
       <UserProfile baseUrl={baseUrl} />
       <AnimatedRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </ConfigProvider>,
 );
