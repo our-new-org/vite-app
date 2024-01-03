6e58fb35f853c48630ea60bd51f7677b5a5a8811
Johan Abde <abdejohan@gmail.com>
2024-01-03 19:56:17 +0100

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,41 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer_content">
-        <h5>SharedNest @ 2024</h5>
-        <h5>Hello there</h5>
+      <div className="footer__content">
+        <div className="footer__content__contact">
+          <h5 className="footer__content__title">© 2023 All rights reserved</h5>
+          <Link to="/">Project Policy</Link>
+          <Link to="/">Terms of Service</Link>
+        </div>
+        <div className="footer__content__contact">
+          <h5 className="footer__content__title">salt.dev</h5>
+        </div>
+        <div className="footer__content__contact">
+          <h5 className="footer__content__title">Made by</h5>
+          <div className="footer__content__github">
+            <GithubOutlined className="footer__content__icon" />
+            <Link
+              to={{ pathname: 'https://github.com/abdejohan' }}
+              target="_blank">
+              Johan
+            </Link>
+          </div>
+          <div className="footer__content__github">
+            <GithubOutlined className="footer__content__icon" />
+            <Link
+              to={{ pathname: 'https://github.com/abdejohan' }}
+              target="_blank">
+              Gayathri
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
 };
+//v
 
 export default Footer;
