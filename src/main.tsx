4a50f060614eaea188db56178556008ef895cbe0
Johan Abde <abdejohan@gmail.com>
2024-01-03 15:03:30 +0100

    Convert back to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AnimatedRoutes from './components/AnimatedRoutes.tsx';
 import Navbar from './components/Navbar.tsx';
 import { ConfigProvider } from 'antd';
@@ -10,10 +10,10 @@ const baseUrl = import.meta.env.PROD ? '/vite-app' : '/';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ConfigProvider
     theme={{ token: { colorPrimary: '#e99ea9', borderRadius: 5 } }}>
-    <HashRouter basename={baseUrl}>
+    <BrowserRouter basename={baseUrl}>
       <Navbar />
       <UserProfile baseUrl={baseUrl} />
       <AnimatedRoutes />
-    </HashRouter>
+    </BrowserRouter>
   </ConfigProvider>,
 );
