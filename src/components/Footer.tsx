b42dea90d11c80c13666e35d0927a4e9f1344bb6
Johan Abde <abdejohan@gmail.com>
2024-01-03 16:33:20 +0100

    Add a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer_content">
+        <h5>SharedNest @ 2024</h5>
+        <h5>Hello there</h5>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
