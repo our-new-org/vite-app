81ba501dc54068333bd1e5fd2a824f3f6178daef
Johan Abde <abdejohan@gmail.com>
2024-01-06 12:45:31 +0100

    Improve slot design

--- src/components/Slot.tsx
+++ src/components/Slot.tsx
@@ -1,5 +1,6 @@
 import { ClockCircleOutlined, RightOutlined } from '@ant-design/icons';
 import { format, addMinutes, getTime } from 'date-fns';
+import { useDatePickerStore } from '../store/datePickerStore';
 
 type SlotProps = {
   slot: Date;
@@ -13,9 +14,10 @@ const Slot = ({
   slot,
   slotDuration,
   handleSlotSelect,
-  selectedSlot,
   disabled = false,
 }: SlotProps) => {
+  const { selectedDate, selectedSlot } = useDatePickerStore();
+
   const handleItemColor = () => {
     const isMatch = getTime(Number(selectedSlot)) === getTime(slot);
     if (!disabled) return isMatch ? '#f8dee1' : '';
@@ -27,8 +29,10 @@ const Slot = ({
       onClick={() => !disabled && handleSlotSelect(slot)}
       style={{ backgroundColor: handleItemColor() }}>
       <div className="slot-item__details">
-        <h5 className="slot-item__details__label">Slot time</h5>
-        <p className="slot-item__details__content">
+        <h5 className="slot-item__details__content">
+          {format(selectedDate, 'EEEE dd/MM')}
+        </h5>
+        <p className="slot-item__details__label">
           <ClockCircleOutlined style={{ marginRight: '5px' }} />
           {`${format(slot, 'HH:mm')} - ${format(
             addMinutes(slot, slotDuration),
